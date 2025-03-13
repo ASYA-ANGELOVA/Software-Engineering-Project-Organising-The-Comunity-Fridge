d67a3e9fc751711c96fdb01c0f3540ecd90ca127
ASYA-ANGELOVA <196176590+ASYA-ANGELOVA@users.noreply.github.com>
2025-03-13 00:24:41 +0000

    Add files via upload

---
+++ Community Fridge Web.go
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name ="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Community Fridge Web App</title>
+        <link rel="stylesheet" href="style.css">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap">
+        </head>
+        <body>
+            <header>
+                <img src="community_logo.png" alt="Community Fridge Logo Image" class="community_logo">
+                <h1>WELCOME TO THE COMMUNITY FRIDGE</h1>
+            </header>
+            <nav>
+                <ul>
+                    <li><a href="index.html">HOME</a></li>
+                    <li><a href="login.html">Login</a></li>
+                    <li><a href="about.html">About</a></li>
+                    <li><a href="contact.html">Contact</a></li>
+                </ul>
+                </nav>
+            <main>
+                <section class="Community">
+                    <img src="community_fridge.jpg" alt="Community Fridge" class ="community_fridge-image">
+                    <h2>Helping to vonerable people,people in inequal situation, they need to know we are here for them</h2>
+                    <p>Join our community
+                        You can help us to help others
+                        You can donate your time voluntering or donating food.</p>
+                </section>
+                </main>
+                <footer>
+                    <p>&copy;Community Fridge. All Rights Reserved.</p>
+                </footer>
+        </body>        
+</html>
